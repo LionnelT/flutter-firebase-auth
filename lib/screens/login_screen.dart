386e29783c8aef3c24d181466e2ece6ff09b0f77
LioTAfro <lionneltsuro56@gmail.com>
2022-02-18 11:22:28 +0200

    Login ui done

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //form key
+  final _formKey = GlobalKey<FormState>();
+
+  //controllers
+
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      // validator: ,'
+      onSaved: (value) {
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.email),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+          )),
+    );
+
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      obscureText: true,
+      keyboardType: TextInputType.emailAddress,
+      // validator: ,'
+      onSaved: (value) {
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.vpn_key_sharp),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+          )),
+    );
+
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(20),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () {},
+        child: const Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      color: Colors.black,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Screen"),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: emailField,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: passwordField,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: loginButton,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Dont have an account"),
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.w500, fontSize: 15),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,22 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign Up page"),
+      ),
+      body: Container(),
+    );
+  }
+}
